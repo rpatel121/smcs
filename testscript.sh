1bc3f486a544cc1f2e8593e7d59b2816441a71d0
rpatel121 <patelriyaz@yahoo.com>
2020-01-09 14:49:37 +0000

    fixed syntax error ver1

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,3 @@ echo "calculate 2 parameter values"
 NUMBER=$(($num1 + $num2))
 echo $NUMBER
 echo "the value is : $NUMBER"
-echo "Added another line"
-NUMBER=$(($num * $num2))
-echo "the value is miltiple : $NUMBER"
